559713949ea6e660608fbce29ffc48dd4abd766f
Pavle Goloskoković <pgoloskokovic@gmail.com>
2016-03-21 17:24:32 +0100

    moved images folder to assets folder

--- workshop 1/js/states/Preload.js
+++ workshop 1/js/states/Preload.js
@@ -4,12 +4,12 @@ Preload.prototype = {
     preload: function () {
         console.log('preload preload');
 
-        this.load.image('background', 'images/background.png');
-        this.load.image('ground', 'images/ground.png');
-        this.load.image('title', 'images/title.png');
-        this.load.image('startButton', 'images/start-button.png');
-        this.load.spritesheet('bird', 'images/bird.png', 34, 24, 3);
-        this.load.spritesheet('pipe', 'images/pipes.png', 54, 320, 2);
+        this.load.image('background', 'assets/images/background.png');
+        this.load.image('ground', 'assets/images/ground.png');
+        this.load.image('title', 'assets/images/title.png');
+        this.load.image('startButton', 'assets/images/start-button.png');
+        this.load.spritesheet('bird', 'assets/images/bird.png', 34, 24, 3);
+        this.load.spritesheet('pipe', 'assets/images/pipes.png', 54, 320, 2);
 
         this.load.onFileComplete.add(function (percent) {
             console.log('loaded ' + percent + "%");
